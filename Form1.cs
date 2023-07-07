c40e296175b3acfb6bd9423f48878e6b9f889984
Srinidhi M <65710007+srinidhim2@users.noreply.github.com>
2023-07-07 18:16:21 +0530

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -14,15 +14,15 @@ namespace Snake
         {
             InitializeComponent();
 
-            //Set settings to default
+            
             new Settings();
 
-            //Set game speed and start timer
+            
             gameTimer.Interval = 1000 / Settings.Speed;
             gameTimer.Tick += UpdateScreen;
             gameTimer.Start();
 
-            //Start New game
+            
             StartGame();
         }
 
@@ -30,10 +30,10 @@ namespace Snake
         {
             lblGameOver.Visible = false;
 
-            //Set settings to default
+            
             new Settings();
 
-            //Create new player object
+            
             Snake.Clear();
             Circle head = new Circle {X = 10, Y = 5};
             Snake.Add(head);
@@ -44,7 +44,7 @@ namespace Snake
 
         }
 
-        //Place random food object
+        
         private void GenerateFood()
         {
             int maxXPos = pbCanvas.Size.Width / Settings.Width;
@@ -57,7 +57,7 @@ namespace Snake
 
         private void UpdateScreen(object sender, EventArgs e)
         {
-            //Check for Game Over
+            
             if (Settings.GameOver)
             {
                 //Check if Enter is pressed
@@ -92,23 +92,23 @@ namespace Snake
             {
                 //Set colour of snake
 
-                //Draw snake
+                
                 for (int i = 0; i < Snake.Count; i++)
                 {
                     Brush snakeColour;
                     if (i == 0)
-                        snakeColour = Brushes.Black;     //Draw head
+                        snakeColour = Brushes.Black;     
                     else
-                        snakeColour = Brushes.Green;    //Rest of body
+                        snakeColour = Brushes.Green;    
 
-                    //Draw snake
+                    
                     canvas.FillEllipse(snakeColour,
                         new Rectangle(Snake[i].X * Settings.Width,
                                       Snake[i].Y * Settings.Height,
                                       Settings.Width, Settings.Height));
 
 
-                    //Draw Food
+                    
                     canvas.FillEllipse(Brushes.Red,
                         new Rectangle(food.X * Settings.Width,
                              food.Y * Settings.Height, Settings.Width, Settings.Height));
@@ -148,11 +148,11 @@ namespace Snake
                     }
 
 
-                    //Get maximum X and Y Pos
+                    
                     int maxXPos = pbCanvas.Size.Width / Settings.Width;
                     int maxYPos = pbCanvas.Size.Height / Settings.Height;
 
-                    //Detect collission with game borders.
+                     
                     if (Snake[i].X < 0 || Snake[i].Y < 0
                         || Snake[i].X >= maxXPos || Snake[i].Y >= maxYPos)
                     {
@@ -160,7 +160,7 @@ namespace Snake
                     }
 
 
-                    //Detect collission with body
+                    
                     for (int j = 1; j < Snake.Count; j++)
                     {
                         if (Snake[i].X == Snake[j].X &&
@@ -170,7 +170,7 @@ namespace Snake
                         }
                     }
 
-                    //Detect collision with food piece
+                   
                     if (Snake[0].X == food.X && Snake[0].Y == food.Y)
                     {
                         Eat();
@@ -179,7 +179,7 @@ namespace Snake
                 }
                 else
                 {
-                    //Move body
+                    
                     Snake[i].X = Snake[i - 1].X;
                     Snake[i].Y = Snake[i - 1].Y;
                 }
@@ -198,7 +198,7 @@ namespace Snake
 
         private void Eat()
         {
-            //Add circle to body
+            
             Circle circle = new Circle
             {
                 X = Snake[Snake.Count - 1].X,
@@ -206,7 +206,7 @@ namespace Snake
             };
             Snake.Add(circle);
 
-            //Update Score
+            
             Settings.Score += Settings.Points;
             lblScore.Text = Settings.Score.ToString();
 
